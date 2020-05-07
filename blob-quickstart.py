b9cd02303b6b3d9af2b8d064ca3cf663f788e59b
noonespecial009 <noonespecial009@github.com>
2020-05-06 17:14:31 -0700

    works

--- blob-quickstart.py
+++ blob-quickstart.py
@@ -24,29 +24,29 @@ try:
     file.write("Hello, World!")
     file.close()
 
-    # # Create a blob client using the local file name as the name for the blob
-    # blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+    # Create a blob client using the local file name as the name for the blob
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
 
-    # print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
 
-    # print("\nListing blobs...")
+    print("\nListing blobs...")
 
-    # # List the blobs in the container
-    # blob_list = container_client.list_blobs()
-    # for blob in blob_list:
-    #     print("\t" + blob.name)
+    # List the blobs in the container
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        print("\t" + blob.name)
 
-    # # Upload the created file
-    # with open(upload_file_path, "rb") as data:
-    #     blob_client.upload_blob(data)
+    # Upload the created file
+    with open(upload_file_path, "rb") as data:
+        blob_client.upload_blob(data)
 
-    # # Download the blob to a local file
-    # # Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
-    # download_file_path = os.path.join(local_path, str.replace(local_file_name ,'.txt', 'DOWNLOAD.txt'))
-    # print("\nDownloading blob to \n\t" + download_file_path)
+    # Download the blob to a local file
+    # Add 'DOWNLOAD' before the .txt extension so you can see both files in the data directory
+    download_file_path = os.path.join(local_path, str.replace(local_file_name ,'.txt', 'DOWNLOAD.txt'))
+    print("\nDownloading blob to \n\t" + download_file_path)
 
-    # with open(download_file_path, "wb") as download_file:
-    #     download_file.write(blob_client.download_blob().readall())
+    with open(download_file_path, "wb") as download_file:
+        download_file.write(blob_client.download_blob().readall())
 
     # # Clean up
     # print("\nPress the Enter key to begin clean up")
